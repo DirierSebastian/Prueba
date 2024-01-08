5ccc03d7c8c2b0beb6f1665ab985986da412caf4
DirierSebastian <sebastianagvzla@gmail.com>
2024-01-08 09:13:36 -0700

    Agregar archivos de proyecto.

---
+++ Prueba/Prueba/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prueba
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            Refre.IsRefreshing = true;
+            Thread.Sleep(1000);
+            Refre.IsRefreshing = false;
+        }
+    }
+}
